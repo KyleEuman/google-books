85631d847a04c5c5a833daa40d52d1031a18abbe
Kyle Euman <euman_kyle@yahoo.com>
2021-06-22 12:45:08 -0400

    put in api code

---
+++ Client/src/utils/API.js
@@ -0,0 +1,25 @@
+/* eslint-disable import/no-anonymous-default-export */
+import axios from "axios";
+
+export default {
+    
+    googleBooks: function(query) {
+        return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`)
+    },
+    
+    saveBook: function(bookData) {
+        return axios.post("/api/books", bookData);
+    },
+    
+    getBooks: function() {
+        return axios.get("/api/books");
+    },
+    
+    getBook: function(id) {
+        return axios.get("/api/books/" + id);
+    },
+    
+    deleteBook: function(id) {
+        return axios.delete("/api/books/" + id);
+    }
+};
